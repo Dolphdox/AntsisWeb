6cf8b5fd4872294375077173390ea4a1bf41aaf6
WCZ <wcz0@outlook.com>
2019-12-06 13:41:31 +0800

    1.0.6
    12/6号

--- js/recommend.js
+++ js/recommend.js
@@ -1,6 +1,22 @@
 
 $("document").ready(function(){
+    //按钮关闭
     $("#close").click(function(){
-        $(this).parents("#dialog-container").hide();
+        $(this).parents("#dialog-container").hide().children().animate({
+            width: "20",
+            height: "15%",
+        }, 200, "swing");
+    });
+
+    $("#vx a").each(function(){
+        $(this).click(function(){
+            //打开对话框
+            $("#dialog-container").show().children().animate({
+                width: "45%",
+                height: "60%",
+            }, 200, "swing");
+            //填充二维码
+            $("<img src='"+")
+        })
     });
 })
