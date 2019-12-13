39dd84b6cf0589f22d3e5f0aa564edac7b6f43a0
WCZ <wcz0@outlook.com>
2019-12-13 21:37:16 +0800

    1.0.2

--- js/recommend.js
+++ js/recommend.js
@@ -10,24 +10,21 @@ $("document").ready(function(){
         $("#content").html("");
     });
     //公众号按钮
-    $("#vx a").each(function(i){
-        $(this).click(function(){
-            var alt=$(this).siblings(".img").children().attr("alt");
-            var src=$(this).siblings(".img").children().attr("src");
-            //处理src字符
-            var str = src.replace(/.jpg|.png/,"");
-            //打开对话框
-            $("#dialog-container").css("background-color","rgb(0, 0, 0, 0.9)").show().children().animate({
-                width: "60%",
-                height: "70%",
-            }, 200, "swing");
-            //填充二维码
-            $("#content").append($("<p>扫描关注公众号</p><div><img src='"+str+"-qrcode.jpg'></div><p>"+alt+"</p>"));
-        })
+    $("#vx a").click(function(){
+        var alt=$(this).siblings(".img").children().attr("alt");
+        var src=$(this).siblings(".img").children().attr("src");
+        //处理src字符
+        var str = src.replace(/.jpg|.png/,"");
+        //打开对话框
+        $("#dialog-container").css("background-color","rgb(0, 0, 0, 0.9)").show().children().animate({
+            width: "60%",
+            height: "70%",
+        }, 200, "swing");
+        //填充二维码
+        $("#content").append($("<p>扫描关注公众号</p><div><img src='"+str+"-qrcode.jpg'></div><p>"+alt+"</p>"));
     });
 
-    $("#bup a").each(function(i){
-        $(this).click(function(){
+    $("#bup a").click(function(){
             var img=$(this).siblings(".img").children().attr("src");
             var name=$(this).siblings("h3").text();
             var p=$(this).siblings("p").text();
@@ -41,25 +38,24 @@ $("document").ready(function(){
             $("<div class='row row-2'><div class='col'><div class='img'><img src="+img+"></div></div><div class='col'><h3>"+name+"</h3></div></div>").appendTo("#content");
             $("<div class='row'><div class='col'><p>"+p+"</p></div></div>").appendTo("#content");
             $("<div class'row'><div class='col'><a class='btn' href='"+url+"'><span>前往主页</span></a></div></div>").appendTo("#content");
-        })
     })
 
-    $("#youtube a").each(function(i){
-        $(this).click(function(){
-            var img=$(this).siblings(".img").children().attr("src");
-            var name=$(this).siblings("h3").text();
-            var p=$(this).siblings("p").text();
-            var url=$(this).data("url");
-            //打开对话框
-            $("#dialog-container").css("background-color","rgb(0, 0, 0, 0.9)").show().children().animate({
-                width: "60%",
-                height: "70%",
-            }, 200, "swing");
-            //填充内容
-            $("<div class='row row-2'><div class='col'><div class='img'><img src="+img+"></div></div><div class='col'><h3>"+name+"</h3></div></div>").appendTo("#content");
-            $("<div class='row'><div class='col'><p>"+p+"</p></div></div>").appendTo("#content");
-            $("<div class'row'><div class='col'><a class='btn' href='"+url+"'><span>前往主页</span></a></div></div>").appendTo("#content");
-        })
+
+    $("#youtube a").click(function(){
+        var img=$(this).siblings(".img").children().attr("src");
+        var name=$(this).siblings("h3").text();
+        var p=$(this).siblings("p").text();
+        var url=$(this).data("url");
+        //打开对话框
+        $("#dialog-container").css("background-color","rgb(0, 0, 0, 0.9)").show().children().animate({
+            width: "60%",
+            height: "70%",
+        }, 200, "swing");
+        //填充内容
+        $("<div class='row row-2'><div class='col'><div class='img'><img src="+img+"></div></div><div class='col'><h3>"+name+"</h3></div></div>").appendTo("#content");
+        $("<div class='row'><div class='col'><p>"+p+"</p></div></div>").appendTo("#content");
+        $("<div class'row'><div class='col'><a class='btn' href='"+url+"'><span>前往主页</span></a></div></div>").appendTo("#content");
+    
     });
     var slider = $(".slider"),
         sC = $(".screen-container"),
@@ -97,5 +93,7 @@ $("document").ready(function(){
     $(window).resize(function(){
         setSize();
     })
+
+    // TODO: 写一个定时器
 })
 
---
+++ js/techniques.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+    $(".col-right .col").click(function(){
+        //获取点击的信息
+        var dImg=$(this).children(".img").children().attr("src"),
+            h=$(this).children("p").text(),
+            dUrl=$(this).data("url");
+        //获取父级信息
+        var iDTitle=$(this).parent(".col-right").siblings(".col-left").find("iframe").data("title"),
+            iDImg=$(this).parent(".col-right").siblings(".col-left").find("iframe").data("img"),
+            iSrc=$(this).parent(".col-right").siblings(".col-left").find("iframe").attr("src");
+        
+        //更改父级消息
+        $(this).parent(".col-right").siblings(".col-left").find("iframe").remove();
+        $(this).parent(".col-right").siblings(".col-left").children().append("<iframe src='"+dUrl+"' scrolling='no' border='0' frameborder='no' framespacing='0' allowfullscreen='true' data-title='"+h+"' data-img='"+dImg+"'></iframe>")
+        
+        //更改点击信息
+        $(this).children(".img").children().remove();
+        $(this).children("p").remove();
+        $(this).data("url", iSrc);
+        $(this).children(".img").append("<img src='"+iDImg+"'>");
+        $(this).append("<p>"+iDTitle+"</p>");
+    });
+    
+    $(window).scroll(function(){
+        var top =$(document).scrollTop();
+        var menu=$(".menu");
+        var item=$(".item");
+        var currentId="";
+    
+        item.each(function(){
+            var itemTop=$(this).offset().top-300;
+            if(top>itemTop){
+                currentId="#"+$(this).attr("id");
+            }else{
+                return false;
+            }
+        });
+        var currentLink=menu.find(".menu-active");
+        if(currentId && currentLink.children().attr("href") != currentId){
+            currentLink.removeClass("menu-active");
+            menu.find("[href='"+currentId+"']").parent(".menu-item").addClass("menu-active");
+        }
+    }); 
+    //给锚点添加过渡动画
+    $(".menu-item>a").click(function(){
+        $("html,body").stop(true, true).animate({
+            scrollTop: ($($(this).attr('href')).offset().top
+            )},1000, "swing");
+    })
+})
